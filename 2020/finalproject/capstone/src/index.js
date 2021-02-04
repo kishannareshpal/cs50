065ae9154bab5c5ea83c02751a7a2e18fe64ed94
Kishan Jadav <kishannareshpal@users.noreply.github.com>
2021-02-04 17:27:45 +0000

    moved

--- 2020/finalproject/capstone/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './Index.stylesheet.scss';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById("app"));
--- 2020/project4/network/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import EmptyPage
-
-def try_or_none(function):
-    try:
-        return function()
-
-    except:
-        return None;
