4d8e5bed936dbfbc53103110ae7aa35617935c94
Kishan Jadav <kishan_jadav@hotmail.com>
2021-08-26 07:20:34 +0100

    fixed the algorithm from previous commit

--- x/week1/pset1/cash/cash.c
+++ x/week1/pset1/cash/cash.c
@@ -17,36 +17,35 @@ int main(void)
     int owed = round(change * 100);
     // Keep track of the number of coins being given.
     int nr_coins = 0;
-    do
+
+    while (owed > 0)
     {
-        // Possible coins are: 25¢, 10¢, 5¢ and 1¢.
+        // Possible coins are: ¢25, ¢10, ¢5 and ¢1.
         if (owed >= 25)
         {
-            // give one quarter (25¢)
+            // give one quarter (¢25)
             owed -= 25;
 
         }
         else if (owed >= 10)
         {
-            // give one dime (10¢)
+            // give one dime (¢10)
             owed -= 10;
 
         }
         else if (owed >= 5)
         {
-            // give one nickel (5¢)
+            // give one nickel (¢5)
             owed -= 5;
 
         }
         else
         {
-            // give one penny (1¢)
+            // give one penny (¢1)
             owed -= 1;
         }
         nr_coins++;
-
     }
-    while (owed != 0);
 
     // Print out the number of coins
     printf("%i\n", nr_coins);
